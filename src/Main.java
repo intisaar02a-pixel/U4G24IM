a3ad9e5bbc0321f8a365656b50c8fc7052dd08f6
intisaar02a-pixel <intisaar02a@gmail.com>
2026-01-09 21:32:06 +0100

    main klassen

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+/**
+ * Huvudklass som startar spelet Omvälvning
+ *
+ * @author Intisaar & Maya
+ * version 1.0
+ */
 
-public class Main {
+    public class Main {
+    /**
+     * Startar applikationen
+     *
+     * @param args kommandoradsargument (används ej)
+     */
     public static void main (String[] args) {
+        new controller.GameController();
 
     }
 }
