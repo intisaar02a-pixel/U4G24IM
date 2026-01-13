e05799b90c2be4b94803dccc03ad303af3891aae
MayaZabur05 <maya.zabur@gmail.com>
2026-01-13 18:08:41 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
-/**
- * Huvudklass som startar spelet Omvälvning
- *
- * @author Intisaar & Maya
- * version 1.0
- */
+
 
     public class Main {
-    /**
-     * Startar applikationen
-     *
-     * @param args kommandoradsargument (används ej)
-     */
     public static void main (String[] args) {
         System.out.println("Hello World!");
 
